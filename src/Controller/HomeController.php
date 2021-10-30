f1842e09744797ebc587e97030940782411e1677
Peter Ton <peterton91@gmail.com>
2021-10-30 10:18:28 +0200

    Fix composer validation messages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
@@ -17,7 +18,7 @@ final class HomeController extends AbstractController
     /**
      * @Route("/")
      */
-    public function number()
+    public function number(): Response
     {
         return $this->render('homepage.html.twig');
     }
