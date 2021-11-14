983788e1c1522c6360ed0cd6533414760619d934
Peter Ton <peterton91@gmail.com>
2021-11-14 15:00:40 +0100

    Fix missing import in controllers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
 {
