6c96dcbae267c50e7f70445fad47c461365b7b9e
Peter Ton <peterton91@gmail.com>
2021-11-19 10:50:48 +0100

    Add artikelen page page

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class BlogController extends AbstractController
+{
+    private $blogPosts = [
+        'high-bar-vs-low-bar-squat' => 'high-bar-low-bar.html.twig',
+        'de-halter' => 'halters.html.twig',
+        'programming-voor-powerlifting' => 'programming.html.twig',
+        'deload' => 'deload.html.twig',
+    ];
+
+
+    /**
+     * @Route("/artikelen")
+     */
+    public function articleListAction(Request $request)
+    {
+        return $this->render('artikelen/list.html.twig');
+    }
+
+    /**
+     * @Route("/artikelen/{slug}", name="article_details")
+     */
+    public function articlePostAction(Request $request, string $slug)
+    {
+        $blogPost = $this->blogPosts[$slug] ?? null;
+
+        if ($blogPost === null) {
+            throw $this->createNotFoundException('Unable to find that article');
+        }
+
+        return $this->render(
+            'artikelen/' . $blogPost,
+            ['activePage' => 'artikelen']
+        );
+    }
+}
