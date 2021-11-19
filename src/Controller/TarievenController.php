4dd4a8ec8939af45185085aa5c67f3e345ffcac7
Peter Ton <peterton91@gmail.com>
2021-11-19 10:36:09 +0100

    Add tarieven page

---
+++ src/Controller/TarievenController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class TarievenController extends AbstractController
+{
+    /**
+     * @Route("/tarieven")
+     */
+    public function showTarievenPage(): Response
+    {
+        return $this->render('tarieven.html.twig');
+    }
+}
