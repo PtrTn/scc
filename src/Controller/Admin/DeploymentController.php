693782f2f051b1a05cdebe1a11088f549719f98b
Peter Ton <peterton91@gmail.com>
2021-11-27 14:20:15 +0100

    Update vendor unpacking script

--- src/Controller/Admin/DeploymentController.php
+++ src/Controller/Admin/DeploymentController.php
@@ -6,29 +6,46 @@ use PhpZip\Exception\ZipException;
 use PhpZip\ZipFile;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeploymentController extends AbstractController
 {
-    private LoggerInterface $logger;
+    private const ZIPPED_VENDOR_FILEPATH = __DIR__.'/../../../deploy/vendor.zip';
+    private const TEMP_VENDOR_DIRECTORY = __DIR__.'/../../../vendor_tmp';
 
-    public function __construct(LoggerInterface $logger)
+    private LoggerInterface $logger;
+    private Filesystem $filesystem;
+
+    public function __construct(LoggerInterface $logger, Filesystem $filesystem)
     {
         $this->logger = $logger;
+        $this->filesystem = $filesystem;
     }
 
     #[Route('/admin/deploy')]
     public function index(): Response
     {
+        try {
+            $this->filesystem->remove(self::TEMP_VENDOR_DIRECTORY);
+            $this->filesystem->mkdir(self::TEMP_VENDOR_DIRECTORY);
+        } catch (IOException $exception) {
+            $this->logger->error('Unable to (re)create temporary vendor directory', ['exception' => $exception]);
+
+            return $this->json('Failed to (re)create temporary vendor directory', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
         $zipFile = new ZipFile();
         try {
             $zipFile
-                ->openFile(__DIR__ . '/../../../deploy/vendor.zip')
-                ->extractTo(__DIR__ . '/../../../vendor_tmp');
+                ->openFile(self::ZIPPED_VENDOR_FILEPATH)
+                ->extractTo(self::TEMP_VENDOR_DIRECTORY);
         }
         catch(ZipException $exception){
             $this->logger->error('Unable to unzip', ['exception' => $exception]);
+
             return $this->json('Failed to unzip vendor', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
         finally{
