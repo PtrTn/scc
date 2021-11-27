36c4975dfc1b3752f9ddf3c3a291e9e1d758664a
Peter Ton <peterton91@gmail.com>
2021-11-27 14:00:00 +0100

    Implement vendor unzipping using deployment endpoint

--- src/Controller/Admin/DeploymentController.php
+++ src/Controller/Admin/DeploymentController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller\Admin;
 
+use PhpZip\Exception\ZipException;
+use PhpZip\ZipFile;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeploymentController extends AbstractController
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     #[Route('/admin/deploy')]
     public function index(): Response
     {
-        return $this->json('hi there');
+        $zipFile = new ZipFile();
+        try {
+            $zipFile
+                ->openFile(__DIR__ . '/../../../deploy/vendor.zip')
+                ->extractTo(__DIR__ . '/../../../vendor_tmp');
+        }
+        catch(ZipException $exception){
+            $this->logger->error('Unable to unzip', ['exception' => $exception]);
+            return $this->json('Failed to unzip vendor', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        finally{
+            $zipFile->close();
+        }
+
+        return $this->json('Successfully unzipped vendor');
     }
 }
